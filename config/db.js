2683c49e446fd67e6d9f106eb1b5da20a87b6771
Lennon-Alan <jhonalexchsucasaire@gmail.com>
2025-07-18 02:42:06 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Necesario para Render
-  }
-});
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false // Necesario para Render
+    }
+  });
 
-pool.connect((err) => {
-  if (err) {
-    console.error('Error conectando a la base de datos:', err.stack);
-  } else {
-    console.log('✅ Conexión exitosa a la base de datos');
-  }
-});
+  pool.connect((err) => {
+    if (err) {
+      console.error('Error conectando a la base de datos:', err.stack);
+    } else {
+      console.log('✅ Conexión exitosa a la base de datos');
+    }
+  });
 
-module.exports = pool;
+  module.exports = pool;
