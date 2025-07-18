489b85ee960dc1d527d2252e5f4df23105d82eb5
Lennon-Alan <jhonalexchsucasaire@gmail.com>
2025-07-18 02:33:38 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,4 +7,12 @@ const pool = new Pool({
   }
 });
 
+pool.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err.stack);
+  } else {
+    console.log('✅ Conexión exitosa a la base de datos');
+  }
+});
+
 module.exports = pool;
