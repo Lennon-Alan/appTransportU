721d6c052181417668d43e6086ad37869fcfab02
Lennon-Alan <jhonalexchsucasaire@gmail.com>
2025-07-18 01:38:48 -0500

    Delete bus-tracker-app directory

--- bus-tracker-app/src/socket/socket.js
+++
@@ -1,7 +0,0 @@
-// src/socket/socket.js
-import { io } from "socket.io-client";
-
-// Solo para pruebas locales (mismo Wi-Fi/red)
-const socket = io("http://192.168.100.7:4000"); // <- usa tu IP local real
-
-export default socket;
